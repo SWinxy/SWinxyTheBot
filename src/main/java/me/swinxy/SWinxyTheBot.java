be72e26de5b815a03b4da9a04378e39439379656
SWinxy <swinxythecat@gmail.com>
2021-08-05 23:18:36 -0600

    nobot check

--- src/main/java/me/swinxy/SWinxyTheBot.java
+++ src/main/java/me/swinxy/SWinxyTheBot.java
@@ -3,6 +3,8 @@ package me.swinxy;
 import net.sourceforge.jwbf.core.actions.HttpActionClient;
 import net.sourceforge.jwbf.core.contentRep.Article;
 import net.sourceforge.jwbf.mediawiki.bots.MediaWikiBot;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.TimeUnit;
 
@@ -21,6 +23,23 @@ public class SWinxyTheBot {
 		MediaWikiBot bot = new MediaWikiBot(client);
 
 		Article article = bot.getArticle("Minecraft");
-		System.out.println(RemoveEnglishCiteParameter.apply(article.getText()));
+		String text = article.getText();
+
+		if (allowBots(text, "SWinxyTheBot")) {
+			System.out.println(RemoveEnglishCiteParameter.apply(text));
+		}
+	}
+
+
+	/**
+	 * Stolen from wikipedia:en:Template:Bots.
+	 * Checks to see if the bot may edit the page.
+	 * @param text article text
+	 * @param user username of the bot
+	 * @return true if the article allows editing from the bot
+	 */
+	@Contract(pure = true)
+	public static boolean allowBots(@NotNull String text, String user) {
+		return !text.matches("(?si).*\\{\\{(nobots|bots\\|(allow=none|deny=([^}]*?" + user + "[^}]*?|all)|optout=all))}}.*");
 	}
 }
